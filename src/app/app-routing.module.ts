fe526e4902216432f5b4c24adae34133a6c6fae6
zampoukos <xboxftw11@hotmail.com>
2022-10-07 22:55:48 +0300

    feat(routing): added routes for favorites and photos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', loadChildren: () => import('./photos/photos.module').then(m => m.PhotosModule)
+
+  },
+  {
+    path: 'favorites', loadChildren: () => import('./favorites/favorites.module').then(m => m.FavoritesModule)
+  },
+
+  // {path: 'photos/:id', component: PhotoComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}

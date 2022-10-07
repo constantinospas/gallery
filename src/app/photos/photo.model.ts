44f6deb0c699c25ae0de4bf5f0440d2f9829601a
zampoukos <xboxftw11@hotmail.com>
2022-10-07 23:10:41 +0300

    feat(model): added photo model

---
+++ src/app/photos/photo.model.ts
@@ -0,0 +1,11 @@
+export interface IPhotoModel {
+  id: number;
+  width: number;
+  author: string;
+  height: number;
+  format: string;
+  filename: string;
+  post_url: string;
+  author_url: string;
+  url?: string;
+}

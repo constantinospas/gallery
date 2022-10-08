f7dcec9feab905aa8e785b4b6c1a6793521621ab
zampoukos <xboxftw11@hotmail.com>
2022-10-08 17:45:08 +0300

    feat(store): localstorage sync

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { StoreModule } from '@ngrx/store';
+import { ActionReducer, ActionReducerMap, MetaReducer, StoreModule } from '@ngrx/store';
 import { HeaderComponent } from './header/header.component';
 import { HttpClientModule } from '@angular/common/http';
 import { favoritesReducer } from './store/favorites/favorites.reducer';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { AppState } from './store/app.state';
+import { localStorageSync } from 'ngrx-store-localstorage';
+
+const reducers: ActionReducerMap<AppState> = { favorites: favoritesReducer };
+
+export function localStorageSyncReducer(reducer: ActionReducer<any>): ActionReducer<any> {
+  return localStorageSync({ keys: ['favorites'], rehydrate: true })(reducer);
+}
+
+const metaReducers: Array<MetaReducer<any, any>> = [localStorageSyncReducer];
 
 @NgModule({
   declarations: [
@@ -20,7 +30,7 @@ import { StoreDevtoolsModule } from '@ngrx/store-devtools';
     AppRoutingModule,
     HttpClientModule,
     BrowserAnimationsModule,
-    StoreModule.forRoot({favorites: favoritesReducer}),
+    StoreModule.forRoot(reducers, { metaReducers }),
     StoreDevtoolsModule.instrument({
       maxAge: 50
     })

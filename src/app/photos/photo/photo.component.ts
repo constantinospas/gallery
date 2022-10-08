008d695e20f7a948b2e8df16ce72b3738230bcd2
zampoukos <xboxftw11@hotmail.com>
2022-10-08 18:42:38 +0300

    chore(photo): move photo component under photos module

--- src/app/photos/photo/photo.component.ts
+++ src/app/photos/photo/photo.component.ts
@@ -2,6 +2,9 @@ import { Component, Input, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ImgComponent } from '../shared/img/img.component';
 import { MatButtonModule } from '@angular/material/button';
+import { Store } from '@ngrx/store';
+import { AppState } from '../store/app.state';
+import { ActivatedRoute, Route, Router } from '@angular/router';
 
 @Component({
   selector: 'app-photo',
@@ -11,9 +14,16 @@ import { MatButtonModule } from '@angular/material/button';
   styleUrls: ['./photo.component.scss']
 })
 export class PhotoComponent implements OnInit {
-  @Input() photo: any = {};
+  photo: any;
 
-  constructor() { }
+  constructor(private store: Store<AppState>, private route: ActivatedRoute) {
+    route.params.subscribe(params => {
+      const id = params['id'];
+      store.select('favorites').subscribe(store => {
+        this.photo = store.favorites[id];
+      });
+    });
+  }
 
   ngOnInit(): void {
   }

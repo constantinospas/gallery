ec064b61f1a7a157dde088128ce07a2c0afcbc06
zampoukos <xboxftw11@hotmail.com>
2022-10-08 03:05:18 +0300

    fix(store): remove load since i will not be using backend

--- src/app/store/favorites/favorites.actions.ts
+++ src/app/store/favorites/favorites.actions.ts
@@ -1,5 +1,4 @@
 import { createAction, props } from '@ngrx/store';
 
-export const loadFavorites = createAction('[Favorites] Load');
 export const addFavorite = createAction('[Favorites] Add', props<{ src: string }>());
 export const removeFavorite = createAction('[Favorites] Remove', props<{ src: string }>());
